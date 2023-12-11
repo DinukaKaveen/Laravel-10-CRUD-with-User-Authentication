fd073f8ebd696c517b39410096c57ae9f6f73025
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-11 11:38:42 +0530

    create routes

--- laravel-crud-app/routes/web.php
+++ laravel-crud-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/products', [ProductController::class, 'get_products'])->name('product.get');
+    Route::post('/create_product', [ProductController::class, 'create_product'])->name('product.create');
+    Route::put('/edit_product', [ProductController::class, 'edit_product'])->name('product.edit');
+    Route::delete('/delete_product', [ProductController::class, 'delete_product'])->name('product.delete');
 });
 
 require __DIR__.'/auth.php';
