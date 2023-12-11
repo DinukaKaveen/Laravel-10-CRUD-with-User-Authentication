4811db35f2cc927996eae0b4432e8844c6e54e1d
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-11 13:50:40 +0530

    create pages

--- laravel-crud-app/resources/views/layouts/navigation.blade.php
+++ laravel-crud-app/resources/views/layouts/navigation.blade.php
@@ -16,6 +16,16 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('product.create')" :active="request()->routeIs('product.create')">
+                        {{ __('Create Product') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('product.edit')" :active="request()->routeIs('product.edit')">
+                        {{ __('Update Product') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
