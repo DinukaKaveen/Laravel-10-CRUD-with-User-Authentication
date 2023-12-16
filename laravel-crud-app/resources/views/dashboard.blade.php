e315ab6dcdcc0751e29d50aa72ebafa956c04734
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-16 20:54:07 +0530

    Update dashboard.blade.php

--- laravel-crud-app/resources/views/dashboard.blade.php
+++ laravel-crud-app/resources/views/dashboard.blade.php
@@ -10,6 +10,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
+                    
 
                     <div class="relative overflow-x-auto">
                         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -36,16 +37,16 @@
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                     <th scope="row"
                                         class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {{ $product->id }}
+                                        {{ $product->name }}
                                     </th>
                                     <td class="px-6 py-4">
-                                        {{ $product->name }}
+                                        {{ $product->qty }}
                                     </td>
                                     <td class="px-6 py-4">
                                         {{ $product->price }}
                                     </td>
                                     <td class="px-6 py-4">
-                                        <a href="#"
+                                        <a href="{{ route('product.edit', ['product' => $product]) }}"
                                             class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                                     </td>
                                 </tr>
